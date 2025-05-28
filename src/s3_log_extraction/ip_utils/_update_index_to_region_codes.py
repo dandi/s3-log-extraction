0e62a1f48785208d45e2827e77ce3d7ff403b9f6
Cody Baker <cody.c.baker.phd@gmail.com>
2025-05-28 08:42:46 -0400

    feat: use batching for IP request

--- src/s3_log_extraction/ip_utils/_update_index_to_region_codes.py
+++ src/s3_log_extraction/ip_utils/_update_index_to_region_codes.py
@@ -23,59 +23,67 @@ def update_index_to_region_codes() -> str | None:
     index_to_ip = load_index_to_ip()
     index_to_region = load_ip_cache(cache_type="index_to_region")
     indices_to_update = set(index_to_ip.keys()) - set(index_to_region.keys())
-    for ip_index in indices_to_update:
-        ip_address = index_to_ip[ip_index]
-        region_code = _get_region_code_from_ip_index(
-            ip_index=ip_index,
-            ip_address=ip_address,
-            ipinfo_handler=ipinfo_handler,
-            index_not_in_services=index_not_in_services,
-        )
-
-        # API limit reached; do not cache and wait for it to reset
-        if region_code == "TBD":
-            continue
-        index_to_region[ip_index] = region_code
+    _update_region_codes_from_ip_indices(
+        ip_indices=indices_to_update,
+        index_to_ip=index_to_ip,
+        index_to_region=index_to_region,
+        index_not_in_services=index_not_in_services,
+        ipinfo_handler=ipinfo_handler,
+    )
 
     indexed_regions_file_path = ip_cache_directory / "index_to_region.yaml"
     with indexed_regions_file_path.open(mode="w") as file_stream:
         yaml.dump(data=index_to_region, stream=file_stream)
 
 
-def _get_region_code_from_ip_index(
-    ip_index: int, ip_address: str, ipinfo_handler: ipinfo.Handler, index_not_in_services: dict[int, bool]
-) -> str:
+def _update_region_codes_from_ip_indices(
+    *,
+    ip_indices: int,
+    index_to_ip: dict[int, str],
+    index_to_region: dict[int, str],
+    index_not_in_services: dict[int, bool],
+    ipinfo_handler: ipinfo.Handler,
+) -> dict[int, str]:
     # Determine if IP address belongs to GitHub, AWS, Google, or known VPNs
     # Azure not yet easily doable; keep an eye on
     # https://learn.microsoft.com/en-us/answers/questions/1410071/up-to-date-azure-public-api-to-get-azure-ip-ranges
     # maybe it will change in the future
-    if index_not_in_services.get(ip_index, None) is None:
-        for service_name in _KNOWN_SERVICES:
-            cidr_addresses_and_subregions = _get_cidr_address_ranges_and_subregions(service_name=service_name)
+    for ip_index in ip_indices:
+        if index_not_in_services.get(ip_index, None) is None:
+            for service_name in _KNOWN_SERVICES:
+                cidr_addresses_and_subregions = _get_cidr_address_ranges_and_subregions(service_name=service_name)
 
-            matched_cidr_address_and_subregion = next(
-                (
-                    (cidr_address, subregion)
-                    for cidr_address, subregion in cidr_addresses_and_subregions
-                    if ipaddress.ip_address(address=ip_address) in ipaddress.ip_network(address=cidr_address)
-                ),
-                None,
-            )
-            if matched_cidr_address_and_subregion is not None:
-                region_service_string = service_name
+                ip_address = index_to_ip[ip_index]
+                matched_cidr_address_and_subregion = next(
+                    (
+                        (cidr_address, subregion)
+                        for cidr_address, subregion in cidr_addresses_and_subregions
+                        if ipaddress.ip_address(address=ip_address) in ipaddress.ip_network(address=cidr_address)
+                    ),
+                    None,
+                )
+                if matched_cidr_address_and_subregion is not None:
+                    region_service_string = service_name
 
-                subregion = matched_cidr_address_and_subregion[1]
-                if subregion is not None:
-                    region_service_string += f"/{subregion}"
-                return region_service_string
-    index_not_in_services[ip_index] = True
+                    subregion = matched_cidr_address_and_subregion[1]
+                    if subregion is not None:
+                        region_service_string += f"/{subregion}"
+                    index_to_region[ip_index] = region_service_string
+        index_not_in_services[ip_index] = True
 
     # Lines cannot be covered without testing on a real IP
-    try:  # pragma: no cover
-        details = ipinfo_handler.getDetails(ip_address=ip_address)
+    # try:  # pragma: no cover
+    ip_address_to_ip_index_not_in_services = {
+        index_to_ip[ip_index]: ip_index
+        for ip_index in ip_indices
+        if index_not_in_services.get(ip_index, False) is False
+    }
+    all_details = ipinfo_handler.getBatchDetails(ip_addresses=list(ip_address_to_ip_index_not_in_services.keys()))
+    for ip_address, details in all_details:
+        details_or_dict = details.details if isinstance(details, ipinfo.details.Details) else details
 
-        country = details.details.get("country", None)
-        region = details.details.get("region", None)
+        country = details_or_dict.get("country", None)
+        region = details_or_dict.get("region", None)
 
         region_string = ""  # Not technically necessary, but quiets the linter
         match (country is None, region is None):
@@ -88,6 +96,8 @@ def _get_region_code_from_ip_index(
             case (False, False):
                 region_string = f"{country}/{region}"
 
-        return region_string
-    except ipinfo.exceptions.RequestQuotaExceededError:  # pragma: no cover
-        return "TBD"
+        ip_index = ip_address_to_ip_index_not_in_services[ip_address]
+        index_to_region[ip_index] = region_string
+    # return index_to_region_code
+    # except ipinfo.exceptions.RequestQuotaExceededError:  # pragma: no cover
+    #     return "TBD"
