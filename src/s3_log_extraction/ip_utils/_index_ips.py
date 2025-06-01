0ac30f8a8c1c38f2b392a102f02351b239570cb1
Cody's Laptop <cody.c.baker.phd@gmail.com>
2025-05-31 22:29:17 -0400

    feat: revert back to hash based indexing, detect collisions, and no longer clean files

--- src/s3_log_extraction/ip_utils/_index_ips.py
+++ src/s3_log_extraction/ip_utils/_index_ips.py
@@ -1,3 +1,5 @@
+import hashlib
+
 import numpy
 import numpy.random
 import tqdm
@@ -15,32 +17,29 @@ def index_ips(*, seed: int = 0) -> None:
 
     The index mapping to full IPs is encrypted and saved to the cache for if access is ever needed for lookup purposes.
     """
-    # Using the upper bound of uint32 as current limit; not expecting radically larger number of users
-    # TODO: add validation to notify if we get close to this
-    index_dtype = numpy.dtype("uint32")
-    rng = numpy.random.default_rng(seed=seed)
-
     cache_directory = get_cache_directory()
     extraction_directory = cache_directory / "extraction"
 
     index_to_ip = load_index_to_ip()
     ip_to_index = {value: key for key, value in index_to_ip.items()}
 
-    available_indices = list(set(range(0, int(numpy.iinfo(index_dtype).max / 4))) - set(index_to_ip.keys()))
-
     full_ip_file_paths = list(extraction_directory.rglob(pattern="*full_ips.txt"))
     for full_ip_file_path in tqdm.tqdm(
         iterable=full_ip_file_paths, total=len(full_ip_file_paths), desc="Indexing IP files", unit="file", smoothing=0
     ):
         full_ips = {ip for ip in numpy.loadtxt(fname=full_ip_file_path, dtype="U15", ndmin=1)}
-        new_ips = full_ips - set(ip_to_index.keys())
+        new_indices_and_uniqueness = {
+            (index := int(hashlib.sha1(string=bytes(ip, "utf-8")).hexdigest()[:7]), 16): index_to_ip.get(index, False)
+            for ip in full_ips
+        }
 
-        new_indices = rng.choice(a=available_indices, size=len(new_ips), replace=False, shuffle=False)
-        available_indices.extend(new_indices)
+        if not all(new_indices_and_uniqueness.values()):
+            message = "Index collision detected - recommend raising the dtype and hash limit."
+            raise ValueError(message)
 
-        for new_ip, new_index in zip(new_ips, new_indices):
-            index_to_ip[int(new_index)] = str(new_ip)
-            ip_to_index[new_ip] = new_index
+        for ip, index in zip(full_ips, new_indices_and_uniqueness.keys()):
+            index_to_ip[int(index)] = str(ip)
+            ip_to_index[ip] = index
 
         full_indexed_ips = [ip_to_index[ip] for ip in full_ips]
 
@@ -51,5 +50,6 @@ def index_ips(*, seed: int = 0) -> None:
     # TODO: add validation for unexpected ip file combinations
     save_index_to_ip(index_to_ip=index_to_ip)
 
-    for full_ip_file_path in full_ip_file_paths:
-        full_ip_file_path.unlink()
+    # TODO: add delayed optional cleanup
+    # for full_ip_file_path in full_ip_file_paths:
+    #     full_ip_file_path.unlink()
